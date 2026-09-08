b5589f31f9fe5ea617f144b6de47f9dd274495a6
mujeebmd691 <ppslords691@gmail.com>
2026-09-08 06:29:17 +0000

    added python program

--- q6.py
+++ q6.py
@@ -1,3 +1,7 @@
 # Read two numbers and print which is greater (use relational operators)
 a = int(input("Enter a number:"))
 b = int(input("Enter another number:"))
+if a > b :
+    print(f"{a} is greater than b.")
+else:
+    print(f"{b} is greater than a.")
