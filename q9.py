49a1b02d93565e7db7c2109bc4713a1b27831a49
mujeebmd691 <ppslords691@gmail.com>
2026-09-08 14:01:27 +0530

    Add age check for voting eligibility

---
+++ q9.py
@@ -0,0 +1,6 @@
+# Read age and check eligibility to vote (age ≥ 18)
+age = int(input("Enter your age:"))
+if age>=18:
+  print("You are eligible to vote.")
+else:
+  print("You are not eligible to vote.")
